8aaf6a7f5444744a30b6e64f7a8c2ece2df91aa7
Oliver Gugger <gugger@gmail.com>
2019-01-05 19:10:27 +0100

    Add test for example in README

--- test/bip-schnorr.js
+++ test/bip-schnorr.js
@@ -79,7 +79,36 @@ describe('test vectors', () => {
       const sigSum2 = bipSchnorr.aggregateSignatures([pk1, pk2, pk3], m);
 
       // when
-      bipSchnorr.verify(P.getEncoded(true), m, sigSum2);
+      let result = false;
+      try {
+        bipSchnorr.verify(P.getEncoded(true), m, sigSum2);
+        result = true;
+      } catch (e) {
+        result = false;
+      }
+      assert.strictEqual(result, true);
+    });
+
+    it('can aggregate and verify example in README', () => {
+      const privateKey1 = BigInteger.fromHex('B7E151628AED2A6ABF7158809CF4F3C762E7160F38B4DA56A784D9045190CFEF');
+      const privateKey2 = BigInteger.fromHex('C90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B14E5C7');
+      const message = Buffer.from('243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89', 'hex');
+      const aggregatedSignature = bipSchnorr.aggregateSignatures([privateKey1, privateKey2], message);
+      assert.strictEqual(aggregatedSignature.toString('hex'), 'd60d7f81c15d57b04f8f6074de17f1b9eef2e0a9c9b2e93550c15b45d6998dc24ef5e393b356e7c334f36cee15e0f5f1e9ce06e7911793ddb9bd922d545b7525');
+
+      // verifying an aggregated signature
+      const publicKey1 = Buffer.from('02DFF1D77F2A671C5F36183726DB2341BE58FEAE1DA2DECED843240F7B502BA659', 'hex');
+      const publicKey2 = Buffer.from('03FAC2114C2FBB091527EB7C64ECB11F8021CB45E8E7809D3C0938E4B8C0E5F84B', 'hex');
+      const sumOfPublicKeys = bipSchnorr.pubKeyToPoint(publicKey1).add(bipSchnorr.pubKeyToPoint(publicKey2));
+      let result = false;
+      try {
+        bipSchnorr.verify(sumOfPublicKeys.getEncoded(true), message, aggregatedSignature);
+        result = true;
+      } catch (e) {
+        result = false;
+      }
+      assert.strictEqual(sumOfPublicKeys.getEncoded(true).toString('hex'), '03f0a6305d39a34582ba49a78bdf38ced935b3efce1e889d6820103665f35ee45b');
+      assert.strictEqual(result, true);
     });
   });
 });
