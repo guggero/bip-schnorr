1abb346544c92202022c64ad61a8369837fbad47
Oliver Gugger <gugger@gmail.com>
2020-12-26 12:21:33 +0100

    taproot: add taprootConstruct

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const schnorr = require('./schnorr');
 schnorr.check = require('./check');
 schnorr.convert = require('./convert');
+schnorr.taproot = require('./taproot');
 
 module.exports = schnorr;
