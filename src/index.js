d256b8325737f84025915b16a2cb42479cf0fd9a
Oliver Gugger <gugger@gmail.com>
2020-12-27 13:45:29 +0100

    package: export math file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const schnorr = require('./schnorr');
 schnorr.check = require('./check');
 schnorr.convert = require('./convert');
+schnorr.math = require('./math');
 schnorr.muSig = require('./mu-sig');
 schnorr.taproot = require('./taproot');
 
