2ae76099169c5f6a2b5aead4dc0f89a7105cf805
Oliver Gugger <gugger@gmail.com>
2022-02-14 11:39:06 +0100

    taproot: revert fix pub key tweak in key spend path

--- src/taproot.js
+++ src/taproot.js
@@ -8,16 +8,15 @@ const concat = Buffer.concat;
 const G = curve.G;
 
 function taprootConstruct(pubKey, scripts) {
-  const Px = convert.intToBuffer(pubKey.affineX);
-
   // If the spending conditions do not require a script path, the output key should commit to an unspendable script path
   // instead of having no script path. This can be achieved by computing the output key point as
   // Q = P + int(hashTapTweak(bytes(P)))G.
   // https://en.bitcoin.it/wiki/BIP_0341#cite_note-22
   if (!scripts) {
-    scripts = [Px];
+    scripts = [];
   }
   const h = taprootTree(scripts);
+  const Px = convert.intToBuffer(pubKey.affineX);
   const P = math.liftX(Px);
   const tweak = convert.bufferToInt(math.taggedHash('TapTweak', concat([Px, h])));
   const Q = P.add(G.multiply(tweak));
@@ -26,10 +25,11 @@ function taprootConstruct(pubKey, scripts) {
 
 function taprootTree(scripts) {
   let h = Buffer.alloc(32, 0);
-  if (scripts.length === 1 && scripts[0].length === 32) {
-    return scripts[0];
+  if (!scripts || scripts.length === 0) {
+    return Buffer.alloc(0, 0);
   }
 
+  // TODO(guggero): Implement script part.
   return h;
 }
 
--- test/schnorr-taproot.spec.js
+++ test/schnorr-taproot.spec.js
@@ -13,7 +13,7 @@ function assertError(error, expectedMessage) {
 
 describe('taproot', () => {
   describe('taprootConstruct', () => {
-    it('can create a tweaked taproot key', () => {
+    it('can create a tweaked taproot key BIP86 test vector 1', () => {
       // given
       const pubKeyBuffer = Buffer.from('03cc8a4bc64d897bddc5fbc2f670f7a8ba0b386779106cf1223c6fc5d7cd6fc115', 'hex');
       const pubKey = ecurve.Point.decodeFrom(curve, pubKeyBuffer);
@@ -22,7 +22,29 @@ describe('taproot', () => {
       const result = taproot.taprootConstruct(pubKey);
 
       // then
-      assert.strictEqual(result.toString('hex'), '7f82c3db962c880194eac332057b4e78d290e19d887e11e600ac846696666e05');
+      assert.strictEqual(result.toString('hex'), 'a60869f0dbcf1dc659c9cecbaf8050135ea9e8cdc487053f1dc6880949dc684c');
+    });
+    it('can create a tweaked taproot key BIP86 test vector 2', () => {
+      // given
+      const pubKeyBuffer = Buffer.from('0283dfe85a3151d2517290da461fe2815591ef69f2b18a2ce63f01697a8b313145', 'hex');
+      const pubKey = ecurve.Point.decodeFrom(curve, pubKeyBuffer);
+
+      // when
+      const result = taproot.taprootConstruct(pubKey);
+
+      // then
+      assert.strictEqual(result.toString('hex'), 'a82f29944d65b86ae6b5e5cc75e294ead6c59391a1edc5e016e3498c67fc7bbb');
+    });
+    it('can create a tweaked taproot key BIP86 test vector 3', () => {
+      // given
+      const pubKeyBuffer = Buffer.from('02399f1b2f4393f29a18c937859c5dd8a77350103157eb880f02e8c08214277cef', 'hex');
+      const pubKey = ecurve.Point.decodeFrom(curve, pubKeyBuffer);
+
+      // when
+      const result = taproot.taprootConstruct(pubKey);
+
+      // then
+      assert.strictEqual(result.toString('hex'), '882d74e5d0572d5a816cef0041a96b6c1de832f6f9676d9605c44d5e9a97d3dc');
     });
   });
 });
