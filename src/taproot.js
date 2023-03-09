a60ada5b1eb4228dbadf61774af66769edd35928
Oliver Gugger <gugger@gmail.com>
2023-03-09 15:49:07 +0100

    schnorr: allow adding of merkle root

--- src/taproot.js
+++ src/taproot.js
@@ -7,30 +7,19 @@ const convert = require('./convert');
 const concat = Buffer.concat;
 const G = curve.G;
 
-function taprootConstruct(pubKey, scripts) {
+function taprootConstruct(pubKey, merkleRoot) {
   // If the spending conditions do not require a script path, the output key should commit to an unspendable script path
   // instead of having no script path. This can be achieved by computing the output key point as
   // Q = P + int(hashTapTweak(bytes(P)))G.
   // https://en.bitcoin.it/wiki/BIP_0341#cite_note-22
-  if (!scripts) {
-    scripts = [];
+  if (!merkleRoot || merkleRoot.length === 0) {
+    merkleRoot = Buffer.alloc(0, 0);
   }
-  const h = taprootTree(scripts);
   const Px = convert.intToBuffer(pubKey.affineX);
   const P = math.liftX(Px);
-  const tweak = convert.bufferToInt(math.taggedHash('TapTweak', concat([Px, h])));
+  const tweak = convert.bufferToInt(math.taggedHash('TapTweak', concat([Px, merkleRoot])));
   const Q = P.add(G.multiply(tweak));
   return convert.intToBuffer(Q.affineX);
-}
-
-function taprootTree(scripts) {
-  let h = Buffer.alloc(32, 0);
-  if (!scripts || scripts.length === 0) {
-    return Buffer.alloc(0, 0);
-  }
-
-  // TODO(guggero): Implement script part.
-  return h;
 }
 
 module.exports = {
